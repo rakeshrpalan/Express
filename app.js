e1ff7b212987ed93009c29c5fe4eb84c0ce8285d
Rakesh <rakesh.r@niveussolutions.com>
2022-11-28 15:12:37 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,51 +1,28 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
-
 const app = express();
+const bodyParse = require('body-parser');
+let cors = require("cors");
+const port = 2000;
+app.use(bodyParse.json())
+app.use(cors());
 
-const port = process.env.PORT || 8080;
-
-app.use(bodyParser.urlencoded({extended:true}))
-
-
-const myObj = {users: [
-    {
-        id:1,
-        name:'roy'
-    },
-    {
-        id:2,
-        name:'john'
-    },{
-        id:3,
-        name:'dani'
-    }
-]}
-
-console.log(myObj);
-
-app.get('/users',(req, res) => {
-    res.json(myObj)
+//Creating a endpoint of get
+app.get('/', (req,res)=>{
+    let query = req.query
+    let path = req.path
+    console.log(query)
+    console.log(path)
+    res.json({"info":{query,path}})
 })
 
-app.get('/user', (req,res) => {
-    res.sendFile(__dirname + '/index.html');
-    
-});
+//Creating a endpoint of post
+app.post('/postData',(req,res)=>{
+    let data = [];
+    data.push(req.body.id)
+    data.push(req.body.name)
+    res.send(data)
+})
 
-app.post('/users', (req,res) => {
-    console.log(req.body);    
-    res.send(`Done : User = ${req.body.user}  &  Id = ${req.body.id}`);
-});
-
-app.listen(port, () => {
-    console.log(`server running at ${port}`);
-});
-
-
-
-
-
-
-
+app.listen(port,()=>{
+    console.log('listening to server http://localhost:',port);
+})
